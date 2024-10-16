e5b008ed4e34484c8830b6cccf94107a4da6889d
shafmaul23 <shafmaul23@gmail.com>
2024-10-16 15:59:50 +0700

    Update code

--- main.js
+++ main.js
@@ -1,2 +1,52 @@
-const books = []
+const books = [];
+const bookForm = document.getElementById('bookForm');
+const bookTitle = document.getElementById('bookFormTitle');
+const bookAuthor = document.getElementById('bookFormAuthor');
+const bookYear = document.getElementById('bookFormYear');
+const bookIsComplete = document.getElementById('bookFormIsComplete');
+const RENDER_EVENT = 'bookListUpdated';
 
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  bookForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    addBook();
+  });
+
+  if(isStorageExist()){
+    loadDataFromStorage();
+  }
+})
+
+function isStorageExist(){
+  if(typeof(Storage) === undefined){
+    alert('Browser kamu tidak mendukung local storage');
+    return false;
+  }
+  return true;
+}
+
+function generateBookObject(id, title, author, year, isComplete){
+  return{
+    id,
+    title,
+    author,
+    year,
+    isComplete
+  };
+}
+
+function addBook(){
+  const title = bookTitle.value;
+  const author = bookAuthor.value;
+  const year = bookYear.value;
+  const isComplete = bookIsComplete.checked;
+
+  const id = generateId();
+
+  const bookObject = generateBookObject(id, title, author, year, isComplete);
+  books.push(bookObject);
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}

c9607e92f19a52a605e737e99e8c2aa7670cdacb
shafmaul23 <shafmaul23@gmail.com>
2024-10-29 23:22:42 +0700

    update

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ const bookTitle = document.getElementById('bookFormTitle');
 const bookAuthor = document.getElementById('bookFormAuthor');
 const bookYear = document.getElementById('bookFormYear');
 const bookIsComplete = document.getElementById('bookFormIsComplete');
-const RENDER_EVENT = 'bookListUpdated';
-
-
+const incompleteBookList = document.getElementById('incompleteBookList');
+const completeBookList = document.getElementById('completeBookList');
+const RENDER_EVENT = 'render-books';
+const SAVED_EVENT = 'saved-books';
+const STORAGE_KEY = 'MABOOK_APPS';
 
 document.addEventListener('DOMContentLoaded', function () {
   bookForm.addEventListener('submit', function(e){
@@ -14,10 +16,9 @@ document.addEventListener('DOMContentLoaded', function () {
     addBook();
   });
 
-  if(isStorageExist()){
-    loadDataFromStorage();
+  if (isStorageExist()){
   }
-})
+});
 
 function isStorageExist(){
   if(typeof(Storage) === undefined){
@@ -38,15 +39,30 @@ function generateBookObject(id, title, author, year, isComplete){
 }
 
 function addBook(){
+  const id = generateId();
   const title = bookTitle.value;
   const author = bookAuthor.value;
-  const year = bookYear.value;
+  const year = parseInt(bookYear.value);
   const isComplete = bookIsComplete.checked;
-
-  const id = generateId();
 
   const bookObject = generateBookObject(id, title, author, year, isComplete);
   books.push(bookObject);
 
   document.dispatchEvent(new Event(RENDER_EVENT));
+
+  saveData();
 }
+
+function generateId(){
+  return +new Date();
+}
+
+function saveData(){
+  parsedData = JSON.stringify(books);
+  localStorage.setItem(STORAGE_KEY, parsedData);
+}
+
+document.addEventListener(RENDER_EVENT, function(){
+  console.log(books);
+  
+})
